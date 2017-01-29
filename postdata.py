fe7bab7e4e98382e953b74b0aea19bc16a966c63
m-ueno <m-ueno@github.com>
2017-01-30 01:22:27 +0900

    run post upload task

--- postdata.py
+++ postdata.py
@@ -3,8 +3,6 @@ import hug
 from subprocess import check_output, call
 from logging import getLogger, StreamHandler, DEBUG, INFO
 
-from tasks import WordCount
-
 log = getLogger(__name__)
 log.setLevel(INFO)
 handler = StreamHandler()
@@ -12,15 +10,21 @@ handler.setLevel(INFO)
 log.addHandler(handler)
 
 
-def count(path: str):
+def generate_thumbnail(path: str):
     """ background job """
-    log.info('count method: {}'.format(path))
-    return call('python tasks.py WordCount --path={}'.format(path), shell=True, universal_newlines=True)
+    call('python tasks.py GenerateThumbnail --ppt-path={}'.format(path),
+         shell=True, universal_newlines=True)
+
+
+def parse_text(path: str):
+    call('python tasks.py parseTxt --ppt-path={}'.format(path),
+         shell=True, universal_newlines=True)
 
 
 def post_upload(path: str):
     log.info('post_upload: {}'.format(path))
-    count(path)
+    parse_text(path)
+    generate_thumbnail(path)
 
 
 @hug.post('/upload')
@@ -29,7 +33,11 @@ def upload_file(user: hug.directives.user, name, upload_file):
     URLを返す
     """
 
-    outfile = os.path.join('uploaded', name.decode('utf-8'))
+    outdir = './upload'
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    outfile = os.path.join(outdir, name.decode('utf-8'))
 
     with open(outfile, 'wb') as f:
         f.write(upload_file)
